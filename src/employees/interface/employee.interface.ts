0442411c24039c2502adc2b73a9dc113e9cd9b09
wellesleymussolini <iei.muss@gmail.com>
2023-05-18 15:21:11 -0300

    Feature - interface

--- src/employees/interface/employee.interface.ts
+++ src/employees/interface/employee.interface.ts
@@ -1,7 +1,5 @@
-import { Document } from 'mongoose';
-
-export interface Employee extends Document {
-    readonly name: string;
-    readonly hired: Boolean;
-    readonly role: string;
+export interface Employee {
+    name: string;
+    hired: Boolean;
+    role: string;
 }
