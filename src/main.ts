b93a180b06ab81e3bf873db0fe9e4549a8243263
wellesleymussolini <iei.muss@gmail.com>
2023-05-18 15:21:59 -0300

    Feature - cors

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,14 @@ import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.int
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Configurações do CORS
   const corsOptions: CorsOptions = {
     origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS',
     preflightContinue: false,
     optionsSuccessStatus: 204,
     credentials: true,
   };
+
   app.enableCors(corsOptions);
 
   await app.listen(3300);
