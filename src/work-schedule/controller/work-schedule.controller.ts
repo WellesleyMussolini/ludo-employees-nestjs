23eee53dbf2f0df42ff348860449e5ab62edca56
wellesleymussolini <iei.muss@gmail.com>
2023-05-23 20:26:57 -0300

    Feature - Controller

--- src/work-schedule/controller/work-schedule.controller.ts
+++ src/work-schedule/controller/work-schedule.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Post, Put, Delete, Req, Res, Param, HttpStatus } from 
 import { Request, Response } from "express";
 import { WorkScheduleService } from '../services/work-schedule.service';
 import { WorkScheduleInterface } from '../interface/work-schedule.interface';
+import { format } from 'date-fns';
 
 @Controller("schedule")
 export class WorkScheduleController {
@@ -23,47 +24,40 @@ export class WorkScheduleController {
     async create(@Req() request: Request, @Res() response: Response) {
         const workScheduleData: WorkScheduleInterface = request.body;
         const { started_at, end_at } = workScheduleData.week_date;
-
+    
         const startDateParts = started_at.split('/');
         const endDateParts = end_at.split('/');
-
+    
         const startDay = Number(startDateParts[0]);
         const startMonth = Number(startDateParts[1]) - 1;
         const startYear = new Date().getFullYear();
-
+    
         const endDay = Number(endDateParts[0]);
         const endMonth = Number(endDateParts[1]) - 1;
         const endYear = new Date().getFullYear();
-
+    
         const startDate = new Date(startYear, startMonth, startDay);
         const endDate = new Date(endYear, endMonth, endDay);
-
-        const monday = startDate.getDate();
-        const tuesday = new Date(startDate.getTime());
-        tuesday.setDate(tuesday.getDate() + 1);
-        const wednesday = new Date(startDate.getTime());
-        wednesday.setDate(wednesday.getDate() + 2);
-        const thursday = new Date(startDate.getTime());
-        thursday.setDate(thursday.getDate() + 3);
-        const friday = new Date(startDate.getTime());
-        friday.setDate(friday.getDate() + 4);
-        const saturday = new Date(startDate.getTime());
-        saturday.setDate(saturday.getDate() + 5);
-        const sunday = endDate.getDate();
-
-        const month = startMonth + 1;
-
+    
+        const monday = format(startDate, 'dd/MM');
+        const tuesday = format(new Date(startDate.getTime() + 1 * 24 * 60 * 60 * 1000), 'dd/MM');
+        const wednesday = format(new Date(startDate.getTime() + 2 * 24 * 60 * 60 * 1000), 'dd/MM');
+        const thursday = format(new Date(startDate.getTime() + 3 * 24 * 60 * 60 * 1000), 'dd/MM');
+        const friday = format(new Date(startDate.getTime() + 4 * 24 * 60 * 60 * 1000), 'dd/MM');
+        const saturday = format(new Date(startDate.getTime() + 5 * 24 * 60 * 60 * 1000), 'dd/MM');
+        const sunday = format(endDate, 'dd/MM');
+    
         const workSchedule: WorkScheduleInterface = {
             week_date: { started_at, end_at },
-            monday: [{ name: workScheduleData.monday[0]?.name || 'Undefined', date: `${monday.toString().padStart(2, '0')}/${month.toString().padStart(2, '0')}` }],
-            tuesday: [{ name: workScheduleData.tuesday[0]?.name || 'Undefined', date: `${tuesday.getDate().toString().padStart(2, '0')}/${month.toString().padStart(2, '0')}` }],
-            wednesday: [{ name: workScheduleData.wednesday[0]?.name || 'Undefined', date: `${wednesday.getDate().toString().padStart(2, '0')}/${month.toString().padStart(2, '0')}` }],
-            thursday: [{ name: workScheduleData.thursday[0]?.name || 'Undefined', date: `${thursday.getDate().toString().padStart(2, '0')}/${month.toString().padStart(2, '0')}` }],
-            friday: [{ name: workScheduleData.friday[0]?.name || 'Undefined', date: `${friday.getDate().toString().padStart(2, '0')}/${month.toString().padStart(2, '0')}` }],
-            saturday: [{ name: workScheduleData.saturday[0]?.name || 'Undefined', date: `${saturday.getDate().toString().padStart(2, '0')}/${month.toString().padStart(2, '0')}` }],
-            sunday: [{ name: workScheduleData.sunday[0]?.name || 'Undefined', date: `${sunday.toString().padStart(2, '0')}/${endMonth.toString().padStart(2, '0')}` }],
+            monday: [{ name: workScheduleData.monday[0]?.name || 'Undefined', date: monday }],
+            tuesday: [{ name: workScheduleData.tuesday[0]?.name || 'Undefined', date: tuesday }],
+            wednesday: [{ name: workScheduleData.wednesday[0]?.name || 'Undefined', date: wednesday }],
+            thursday: [{ name: workScheduleData.thursday[0]?.name || 'Undefined', date: thursday }],
+            friday: [{ name: workScheduleData.friday[0]?.name || 'Undefined', date: friday }],
+            saturday: [{ name: workScheduleData.saturday[0]?.name || 'Undefined', date: saturday }],
+            sunday: [{ name: workScheduleData.sunday[0]?.name || 'Undefined', date: sunday }],
         };
-
+    
         const createdWorkSchedule = await this.workScheduleService.create(workSchedule);
         return response.status(HttpStatus.CREATED).json(createdWorkSchedule);
     }
