6862056d0a8088f817be0fe5768fbe14c1bd0caf
wellesleymussolini <iei.muss@gmail.com>
2023-05-23 20:33:33 -0300

    Feature - Controller

--- src/work-schedule/controller/work-schedule.controller.ts
+++ src/work-schedule/controller/work-schedule.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Post, Put, Delete, Req, Res, Param, HttpStatus } from 
 import { Request, Response } from "express";
 import { WorkScheduleService } from '../services/work-schedule.service';
 import { WorkScheduleInterface } from '../interface/work-schedule.interface';
-import { format } from 'date-fns';
+import { format, parse } from 'date-fns';
 
 @Controller("schedule")
 export class WorkScheduleController {
@@ -24,29 +24,18 @@ export class WorkScheduleController {
     async create(@Req() request: Request, @Res() response: Response) {
         const workScheduleData: WorkScheduleInterface = request.body;
         const { started_at, end_at } = workScheduleData.week_date;
-    
-        const startDateParts = started_at.split('/');
-        const endDateParts = end_at.split('/');
-    
-        const startDay = Number(startDateParts[0]);
-        const startMonth = Number(startDateParts[1]) - 1;
-        const startYear = new Date().getFullYear();
-    
-        const endDay = Number(endDateParts[0]);
-        const endMonth = Number(endDateParts[1]) - 1;
-        const endYear = new Date().getFullYear();
-    
-        const startDate = new Date(startYear, startMonth, startDay);
-        const endDate = new Date(endYear, endMonth, endDay);
-    
-        const monday = format(startDate, 'dd/MM');
-        const tuesday = format(new Date(startDate.getTime() + 1 * 24 * 60 * 60 * 1000), 'dd/MM');
-        const wednesday = format(new Date(startDate.getTime() + 2 * 24 * 60 * 60 * 1000), 'dd/MM');
-        const thursday = format(new Date(startDate.getTime() + 3 * 24 * 60 * 60 * 1000), 'dd/MM');
-        const friday = format(new Date(startDate.getTime() + 4 * 24 * 60 * 60 * 1000), 'dd/MM');
-        const saturday = format(new Date(startDate.getTime() + 5 * 24 * 60 * 60 * 1000), 'dd/MM');
-        const sunday = format(endDate, 'dd/MM');
-    
+
+        const startDate = parse(started_at, 'dd/MM/yyyy', new Date());
+        const endDate = parse(end_at, 'dd/MM/yyyy', new Date());
+
+        const monday = format(startDate, 'dd/MM/yyyy');
+        const tuesday = format(new Date(startDate.getTime() + 1 * 24 * 60 * 60 * 1000), 'dd/MM/yyyy');
+        const wednesday = format(new Date(startDate.getTime() + 2 * 24 * 60 * 60 * 1000), 'dd/MM/yyyy');
+        const thursday = format(new Date(startDate.getTime() + 3 * 24 * 60 * 60 * 1000), 'dd/MM/yyyy');
+        const friday = format(new Date(startDate.getTime() + 4 * 24 * 60 * 60 * 1000), 'dd/MM/yyyy');
+        const saturday = format(new Date(startDate.getTime() + 5 * 24 * 60 * 60 * 1000), 'dd/MM/yyyy');
+        const sunday = format(endDate, 'dd/MM/yyyy');
+
         const workSchedule: WorkScheduleInterface = {
             week_date: { started_at, end_at },
             monday: [{ name: workScheduleData.monday[0]?.name || 'Undefined', date: monday }],
@@ -57,7 +46,7 @@ export class WorkScheduleController {
             saturday: [{ name: workScheduleData.saturday[0]?.name || 'Undefined', date: saturday }],
             sunday: [{ name: workScheduleData.sunday[0]?.name || 'Undefined', date: sunday }],
         };
-    
+
         const createdWorkSchedule = await this.workScheduleService.create(workSchedule);
         return response.status(HttpStatus.CREATED).json(createdWorkSchedule);
     }
