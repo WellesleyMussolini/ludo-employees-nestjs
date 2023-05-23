57ba4dcc11e51a6eee183f07fa795ffdad63c4de
wellesleymussolini <iei.muss@gmail.com>
2023-05-23 18:44:32 -0300

    Feature - interface

--- src/work-schedule/interface/work-schedule.interface.ts
+++ src/work-schedule/interface/work-schedule.interface.ts
@@ -1,38 +1,13 @@
-interface Employee {
-    name: string;
-}
-
 export interface WorkScheduleInterface {
-    monday: Employee[];
-    tuesday: Employee[];
-    wednesday: Employee[];
-    thursday: Employee[];
-    friday: Employee[];
-    saturday: Employee[];
-    sunday: Employee[];
+	week_date: {
+		started_at: string;
+		end_at: string;
+	};
+	monday: { name: string; date: string }[];
+	tuesday: { name: string; date: string }[];
+	wednesday: { name: string; date: string }[];
+	thursday: { name: string; date: string }[];
+	friday: { name: string; date: string }[];
+	saturday: { name: string; date: string }[];
+	sunday: { name: string; date: string }[];
 }
-
-
-[
-	{
-		"_id": "646d2010fb48fd7e35faf3b1",
-		"week_date": {
-            "started_at": "21/05",
-            "end_at": "27/05" 
-        },
-        "monday": [
-		],
-		"tuesday": [
-		],
-		"wednesday": [
-		],
-		"thursday": [
-		],
-		"friday": [
-		],
-		"saturday": [
-		],
-		"sunday": [
-		],
-	}
-]
