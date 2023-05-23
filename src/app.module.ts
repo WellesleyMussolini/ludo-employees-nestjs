44b0fcee979631f9d053b725b9a83d5fcaa4ae49
wellesleymussolini <iei.muss@gmail.com>
2023-05-23 17:26:41 -0300

    Feature - app modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmployeesModule } from './employees/module/employees.module';
+import { WorkScheduleModule } from './work-schedule/module/work-schedule.module';
 
 @Module({
-  imports: [EmployeesModule],
+  imports: [EmployeesModule, WorkScheduleModule],
   controllers: [AppController],
   providers: [AppService],
 })
